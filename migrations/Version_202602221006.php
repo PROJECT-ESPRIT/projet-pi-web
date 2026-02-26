f0853b90bffd2dbb95092ba8adae4fe7fdd787a0
yasminemaatougui <yasmine.maatougui@esprit.tn>
2026-02-27 00:13:13 +0100

    Revert "Merge charity with main (#41)" (#42)
    This reverts commit 24bda1da1d2d41a549644b6de0bab7829e5ff89b.

---
+++ migrations/Version_202602221006.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version_202602221006 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Produit entity';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('CREATE TABLE produit (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, prix DOUBLE PRECISION NOT NULL, stock INT NOT NULL, image VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE produit');
+    }
+}
